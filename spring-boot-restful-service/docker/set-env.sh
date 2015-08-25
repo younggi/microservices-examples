c90cd702b454e21bc8af2f5853d7dd79de7f530b
Younggi Kim <roadlamp@gmail.com>
2015-08-25 23:41:54 +0900

    fixed miss typing

--- spring-boot-restful-service/docker/set-env.sh
+++ spring-boot-restful-service/docker/set-env.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 #export JAVA_HOME=$(/usr/libexec/java_home -v 1.7)
-export DOCKER_HOST_IP=`hostname -I | cut -d' ' -f1'`
+export DOCKER_HOST_IP=`hostname -I | cut -d' ' -f1`
 
 export SPRING_DATA_MONGODB_URI=mongodb://${DOCKER_HOST_IP}/userregistration
 export SPRING_RABBITMQ_HOST=${DOCKER_HOST_IP}
